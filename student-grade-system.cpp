1caf59c2837c2e9cb5860b963aee3b582e6f8bb7
Mazen Hameed <mazenhameed2004@gmail.com>
2026-05-28 18:56:08 +0300

    Update student-grade-system.cpp

--- student-grade-system.cpp
+++ student-grade-system.cpp
@@ -1,21 +1,3 @@
-/*
-========================================
-   Student Grades Management System
-========================================
-
-A simple C++ project for managing student grades.
-
-Features:
-- Add a new student
-- Calculate total, average, and grade
-- Determine student status (Passed/Failed)
-- Search student by seat number
-
-Developer: Mazen Hameed 
-Language : C++
-========================================
-*/
-
 #include <iostream>
 #include <string>
 #include <iomanip>
